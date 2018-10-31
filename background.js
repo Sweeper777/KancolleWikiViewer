5f51daf02045b3c4fcd862e6bb76debe858c0c99
Sweeper777 <sumulang@gmail.com>
2018-10-31 09:15:45 +0000

    implemented quests command

---
+++ background.js
@@ -0,0 +1,14 @@
+var questRegex = /^\s*([abcd]\d+)\s*$/i
+
+function input(text) {
+    if (match = questRegex.exec(text)) {
+        quest(match[1])
+    }
+}
+
+function quest(questID) {
+    var url = "https://kancolle.wikia.com/wiki/Quests#" + questID.toUpperCase();
+    chrome.tabs.update({url: url})
+}
+
+chrome.omnibox.onInputEntered.addListener(input)
